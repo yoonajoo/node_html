9f53f2021ad5ca2fd537d136d41003af074c8be0
yoonajoo <judy0_1@naver.com>
2021-03-17 13:46:50 +0900

    third

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let express = require('express');   // 1,2번 코드는 웹 서버를 돌려주는 코드
+let express = require('express');   
 let app = express();
-let router = require('./router/main')(app); // main 실행
-let port = process.env.PORT || 3000;    // 3000번 포트를 사용하겠다
+let router = require('./router/main')(app);  
+let port = process.env.PORT || 3000;    
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static('public'));  // public 폴더 지정
+app.use(express.static('public'));   
 
 let server = app.listen(port, function(){
     console.log("Express server has started on port "+ port)
